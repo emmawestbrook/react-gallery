ca500fdb2d2ff0e9c8482bbb075723cd3b3259e0
Emma Westbrook <emmalwestbrook@gmail.com>
2020-09-20 19:24:56 -0500

    sql queries now sent to database for both PUT and GET

--- database.sql
+++ database.sql
@@ -18,13 +18,3 @@ VALUES ('3', 'images/potato.jpg', 'my online mahjong avatar', '0');
 
 INSERT INTO "galleryItems" ("id", "path", "description", "likes")
 VALUES ('4', 'iimages/sun.jpg', 'a sun i tattooed on a melon', '0');
-
-
-
--- const galleryItems = [
---     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
---     { id: 2, path: 'images/olga.jpg', description: 'olga of kiev', likes: 0 },
---     { id: 3, path: 'images/potato.jpg', description: 'my online mahjong avatar', likes: 0 },
---     { id: 4, path: 'images/sun.jpg', description: 'a sun i tattooed on a melon', likes: 0 }
-
--- ];
